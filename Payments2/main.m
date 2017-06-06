f3c75a4e238477544c92de7a8e8b5c67ec73c77b
Shreve-La <shrevela@gmail.com>
2017-06-06 11:14:55 -0400

    Initial Commit

---
+++ Payments2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Payments2
+//
+//  Created by swcl on 2017-06-06.
+//  Copyright © 2017 Shreve.ca. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
